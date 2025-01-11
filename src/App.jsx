8029c6b775213bb0215792885cafce99c228ae51
ixone0 <jasonboy025@gmail.com>
2025-01-11 18:16:04 +0700

    get mockapi, fetch, can delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import Image from './components/Image.jsx'
-import Checkbox from './components/Checkbox.jsx'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const BASE_URL = 'https://678247f2c51d092c3dcedd24.mockapi.io'
 
 function App() {
 
-  const todoList = [
-    {
-      text: 'coding react',
-      isChecked: false
-    },
-    {
-      text: 'doing document react',
-      isChecked: true
-    },
-    {
-      text: 'test react',
-      isChecked: false
-    }
-  ]
-  return (
-    <div>
-      {
-        todoList.map((todo, index) => (
-            <Checkbox 
-              key={index}
-              text={todo.text}
-              isChecked={todo.isChecked}
-            />
+  const [todos, setTodos] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
+  async function fetchTodo() {
+     try {
+      const response = await axios.get(`${BASE_URL}/todos`)
+      setTodos(response.data)
+      setIsLoading(false)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  async function deleteTodo(id) {
+      try {
+        setIsLoading(true)
+        await axios.delete(`${BASE_URL}/todos/${id}`)
+        await fetchTodo()
+        setIsLoading(false)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+  useEffect(() => {
+    fetchTodo()
+  }, [])
+
+  return (
+    <>
+    { isLoading && (<div>Loading...</div>) }
+    { !isLoading && <div>
+      {
+        todos.map((todo, index) => (
+          <div key={index}>
+            {todo.id} {todo.name} {todo.status} 
+            <button>Edit</button>
+            <button 
+              onClick={async () => {
+                await deleteTodo(todo.id)
+              }}
+            >Delete</button>
+          </div>
         ))
       }
     </div>
+    }
+    </>
   )
 }
 
